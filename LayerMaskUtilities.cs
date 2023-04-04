d9f29446eafc85f97af7b905d9bb68ce165e2441
Rovsau <71842834+Rovsau@users.noreply.github.com>
2023-04-04 21:12:35 +0200

    Create LayerMaskUtilities.cs

---
+++ LayerMaskUtilities.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public static class LayerMaskUtilities
+{
+    public static string MaskToHexadecimalString(LayerMask layerMask)
+    {
+        return string.Format($"0x{layerMask.value:X}");
+    }
+
+    public static string MaskToBinaryString(LayerMask layerMask)
+    {
+        //return Convert.ToString(layerMask, 2).PadLeft(32, '0');
+        // StringBuilder would allow for "easier" conversion post return, but also add the need for System.Text.
+        int mask = layerMask.value;
+        char[] bits = new char[32];
+        for (int i = 0; i < bits.Length; i++)
+        {
+            bits[bits.Length - 1 - i] = ((mask >> i) & 1) == 1 ? '1' : '0';
+        }
+        return new string(bits);
+    }
+
+    public static int[] GetLayerIndexesFromMask(LayerMask layerMask)
+    {
+        int[] layerIndexes = new int[32];
+        int index = default;
+        for (int i = 0; i < layerIndexes.Length; i++)
+        {
+            if (((1 << i) & layerMask) != 0)
+            {
+                layerIndexes[index++] = i;
+            }
+        }
+        System.Array.Resize(ref layerIndexes, index);
+        return layerIndexes;
+    }
+}
